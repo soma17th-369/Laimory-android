05d7f5f50994f919324b667835d14470a0e96c0e
hyeongseon <hsun5163@gmail.com>
2026-07-20 22:42:59 +0900

    refactor: 초안 범위 DTO 파일 분리 (#165)

--- core/data/src/main/java/com/soma369/laimory/core/data/model/timeline/request/CreateDraftTaskRequest.kt
+++ core/data/src/main/java/com/soma369/laimory/core/data/model/timeline/request/CreateDraftTaskRequest.kt
@@ -18,13 +18,6 @@ data class CreateDraftTaskRequest(
     val sourceItems: List<SourceItemDto>,
 )
 
-/** 서버가 초안 생성에 사용할 오프셋 없는 로컬 datetime 반열린 구간 `[startTime, endTime)`. */
-@Serializable
-data class TimelineWindowDto(
-    val startTime: String,
-    val endTime: String,
-)
-
 /**
  * 초안 요청의 원천 아이템 1건.
  *
